47621fb464c6750334895b0237663912c798a2ac
andreus-dot-focus <vorkozhokovandrey@gmail.com>
2021-04-03 17:13:08 +0700

    Добавьте файлы проекта.

---
+++ SIMS-7/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace SIMS_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.TrainingsLabel = new System.Windows.Forms.Label();
+            this.TrainingsTB = new System.Windows.Forms.TextBox();
+            this.QualityTB = new System.Windows.Forms.TextBox();
+            this.QualityLabel = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.yearLabel = new System.Windows.Forms.Label();
+            this.monthLabel = new System.Windows.Forms.Label();
+            this.dayLabel = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TrainingsLabel
+            // 
+            this.TrainingsLabel.AutoSize = true;
+            this.TrainingsLabel.Location = new System.Drawing.Point(12, 13);
+            this.TrainingsLabel.Name = "TrainingsLabel";
+            this.TrainingsLabel.Size = new System.Drawing.Size(106, 13);
+            this.TrainingsLabel.TabIndex = 0;
+            this.TrainingsLabel.Text = "Кол-во тренировок:";
+            // 
+            // TrainingsTB
+            // 
+            this.TrainingsTB.Location = new System.Drawing.Point(189, 13);
+            this.TrainingsTB.Name = "TrainingsTB";
+            this.TrainingsTB.Size = new System.Drawing.Size(119, 20);
+            this.TrainingsTB.TabIndex = 1;
+            // 
+            // QualityTB
+            // 
+            this.QualityTB.Location = new System.Drawing.Point(189, 50);
+            this.QualityTB.Name = "QualityTB";
+            this.QualityTB.Size = new System.Drawing.Size(119, 20);
+            this.QualityTB.TabIndex = 3;
+            // 
+            // QualityLabel
+            // 
+            this.QualityLabel.AutoSize = true;
+            this.QualityLabel.Location = new System.Drawing.Point(12, 50);
+            this.QualityLabel.Name = "QualityLabel";
+            this.QualityLabel.Size = new System.Drawing.Size(174, 13);
+            this.QualityLabel.TabIndex = 2;
+            this.QualityLabel.Text = "Средняя квалификация состава:";
+            this.QualityLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(663, 41);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(116, 30);
+            this.StartButton.TabIndex = 4;
+            this.StartButton.Text = "Старт";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(314, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Месяц:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(320, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "День:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(329, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Год:";
+            // 
+            // yearLabel
+            // 
+            this.yearLabel.AutoSize = true;
+            this.yearLabel.Location = new System.Drawing.Point(363, 87);
+            this.yearLabel.Name = "yearLabel";
+            this.yearLabel.Size = new System.Drawing.Size(13, 13);
+            this.yearLabel.TabIndex = 10;
+            this.yearLabel.Text = "—";
+            // 
+            // monthLabel
+            // 
+            this.monthLabel.AutoSize = true;
+            this.monthLabel.Location = new System.Drawing.Point(363, 50);
+            this.monthLabel.Name = "monthLabel";
+            this.monthLabel.Size = new System.Drawing.Size(13, 13);
+            this.monthLabel.TabIndex = 9;
+            this.monthLabel.Text = "—";
+            // 
+            // dayLabel
+            // 
+            this.dayLabel.AutoSize = true;
+            this.dayLabel.Location = new System.Drawing.Point(363, 13);
+            this.dayLabel.Name = "dayLabel";
+            this.dayLabel.Size = new System.Drawing.Size(13, 13);
+            this.dayLabel.TabIndex = 8;
+            this.dayLabel.Text = "—";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "hggh";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(15, 103);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "hggh";
+            series1.Name = "Среднее число пожаров в месяц";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(764, 335);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.yearLabel);
+            this.Controls.Add(this.monthLabel);
+            this.Controls.Add(this.dayLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.QualityTB);
+            this.Controls.Add(this.QualityLabel);
+            this.Controls.Add(this.TrainingsTB);
+            this.Controls.Add(this.TrainingsLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TrainingsLabel;
+        private System.Windows.Forms.TextBox TrainingsTB;
+        private System.Windows.Forms.TextBox QualityTB;
+        private System.Windows.Forms.Label QualityLabel;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label yearLabel;
+        private System.Windows.Forms.Label monthLabel;
+        private System.Windows.Forms.Label dayLabel;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ SIMS-7/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SIMS_7
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+
+        int fireCount;
+
+        int day = 0;
+        int month = 0;
+        int year = 0;
+
+        int trainings;
+        int quality;
+        double SF;
+
+        int SO = 120;
+
+        int fire;
+        int human=100;
+        int money=1000;
+
+        double KTP;
+
+        double B;
+        double inB;
+        double outB;
+
+        double Z = 0.2;
+
+        double CH;
+        double inCH;
+        double outCH;
+
+        double PR = 1;
+
+        int P=20;
+        int outP=0; //пожарники тушат все пожары
+        int inP;
+        public Form1()
+        {
+            InitializeComponent();
+            chart1.Series[0].Points.Clear();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            trainings = Convert.ToInt32(TrainingsTB.Text);
+            quality = Convert.ToInt32(QualityTB.Text);
+
+            SF = 4 + (trainings + quality) * 0.000001;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            day++;
+          
+            if (day == 30)
+            {
+                day = 0;
+                chart1.Series[0].Points.AddXY(month, fireCount/30);
+                fireCount = 0;
+                month++;
+                if (month == 12)
+                {
+                    chart1.Series[0].Points.Clear();
+
+                    month = 0;
+                    year++;
+                    inB = money * Z;
+                    B = B + inB;
+
+                    outCH = rnd.Next(10, 30);
+                }
+            }
+
+            dayLabel.Text = day.ToString();
+            monthLabel.Text = month.ToString();
+            yearLabel.Text = year.ToString();
+
+            human += 100;
+
+
+            PR = 0.1*rnd.Next(1,10);
+            B -= PR*B;
+            
+            outB = B;
+
+            inCH = human / SO;
+            CH = inCH - outCH;
+
+            fire = rnd.Next(2, 100);
+            KTP = SF * CH * B * 0.01;
+            inP = Convert.ToInt32(Math.Round(fire + KTP));
+            P = inP - outP;
+            fireCount += P;
+            Console.WriteLine(P);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
